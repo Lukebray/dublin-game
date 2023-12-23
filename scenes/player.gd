b55da3072ea7eaea905e66df45d6450b31d5eddb
Lukebray <39488446+Lukebray@users.noreply.github.com>
2023-12-23 16:31:19 +0000

    basic movement and map

---
+++ scenes/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@onready var velocity_component = $VelocityComponent as VelocityComponent
+
+var base_speed = 0
+
+func _ready():
+	base_speed = velocity_component.max_speed
+
+
+func _process(_delta):
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	velocity_component.accelerate_in_direction(direction)
+	velocity_component.move(self)
+
+
+func get_movement_vector():
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	return Vector2(x_movement, y_movement)
